8047e430b3c186f61ada6455d832edaa929afffe
himanshu-kitey <himanshukitey@gmail.com>
2024-10-28 21:24:45 +0530

    add new file

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 This is my first repository.
 <br>
 Author - Himanshu Kitey
+<br>
+Embedded System Engineer.
---
+++ test.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main(void)
+{
+    printf("Learning Git and GitHub\n");
+    return 0;
+}
